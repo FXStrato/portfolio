f2ef152f0a140beb50efe88bc95b17215d2518c8
Jeff Zhong <jz5822@att.com>
2018-03-19 15:44:27 -0700

    Added hero charts for owrank

--- src/OWRank.js
+++ src/OWRank.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { Row, Col, Input, Card, Avatar, Spin } from 'antd';
+import { Row, Col, Input, Card, Spin, Form, Tooltip, Icon, Avatar } from 'antd';
 import Img from 'react-image';
+import ProgressiveImage from 'react-progressive-image-loading';
+import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis, Cell, LabelList } from 'recharts';
 const Search = Input.Search;
-
+const FormItem = Form.Item;
 
 class OWRank extends Component {
 
@@ -12,6 +14,7 @@ class OWRank extends Component {
     showData: false,
     noComp: false,
     data: null,
+    error: {}
   }
 
   async getData(id) {
@@ -21,16 +24,16 @@ class OWRank extends Component {
     try {
       const response = await fetch(url);
       return response.json().then(res => {
-        if(res.us !== undefined) {
+        if (res.us !== undefined) {
           //Check if they are competitive or not
-          if(res.us.stats.competitive) {
+          if (res.us.stats.competitive) {
             this.setState({ loading: false, showData: true, data: res.us });
           } else {
-            this.setState({loading: false, noComp: true, showData: false, data: null,});
+            this.setState({ loading: false, noComp: true, showData: false, data: null, });
           }
         } else {
           console.log('Invalid ID');
-          this.setState({loading: false, showData: false, data: null});
+          this.setState({ loading: false, showData: false, data: null });
         }
       });
     } catch (err) {
@@ -38,19 +41,53 @@ class OWRank extends Component {
     }
   }
 
-  render() {
-    let rank, hero;
-    if(this.state.data)  {
-      rank = this.state.data.stats.competitive.overall_stats;
-      hero = this.state.data.heroes.stats.competitive;
+  getHeroesData = () => {
+    let temp = [];
+    let data = this.state.data.heroes.stats.competitive;
+    for (let elem in data) {
+      let played = data[elem].general_stats.time_played;
+      let minutes = Math.floor(played * 60);
+      temp.push({ name: this.capitalize(elem), time: played, minutes })
     }
+    temp.sort((a, b) => { return (a.time > b.time) ? -1 : ((b.time > a.time) ? 1 : 0); });
+    return temp;
+  }
+
+  checkSearch = val => {
+    let pattern = new RegExp('^(.+)-([0-9]+)$', 'g');
+    if (pattern.test(val)) this.getData(val);
+    else {
+      let error = {
+        status: 'error',
+        msg: 'Invalid entry, please double check input'
+      }
+      this.setState({ error });
+    }
+  }
+
+  capitalize = val => {
+    return val.charAt(0).toUpperCase() + val.substring(1);
+  }
+
+  render() {
+    let rank, heroes;
+    if (this.state.data) {
+      rank = this.state.data.stats.competitive.overall_stats;
+      heroes = this.getHeroesData();
+    }
+
     return (<div>
       <Row>
         <Col md={24} lg={{span: 12, offset: 6}}>
           <h2>OWRank</h2>
-          <p style={{fontSize: '0.95rem'}}>Search up a US player's competitive stats for this season. Write the input as [Battlenet Name]-[Battlenet ID]</p>
+          <p style={{fontSize: '0.95rem'}}>Search up a US player's competitive stats for the current season of Overwatch. Write the input as [Battlenet Name]-[Battlenet ID]</p>
+          <p>NOTE: The search is case sensitive!</p>
           <Spin spinning={this.state.loading}>
-            <Search placeholder="ex. Neonine-1943" onSearch={value => this.getData(value)} enterButton="Check Rank" size="large" />
+            <Form>
+              <FormItem validateStatus={this.state.error.status} help={this.state.error.msg}>
+                <Search placeholder="ex. Neonine-1943" onSearch={this.checkSearch} onChange={val => this.setState({error: {}})} enterButton="Check Rank" size="large" />
+              </FormItem>
+            </Form>
           </Spin>
         </Col>
       </Row>
@@ -58,16 +95,39 @@ class OWRank extends Component {
       <div>
         <Row style={{marginTop: 30}} type="flex" justify="center" align="middle" gutter={16}>
           <Col sm={12} md={12} lg={4}>
-            <Card
-              loading={this.state.loading}
-              style={{ width: '100%'}}
-              cover={<Img className="responsive-img center-align" style={{maxWidth: 300, marginLeft: 'auto', marginRight: 'auto'}} alt={rank.tier} src={rank.tier_image} />}
-            >
-              <p>Card information goes here</p>
+            <Card loading={this.state.loading} className="full-width">
+              <Row gutter={16} type="flex" align="middle">
+                <Col md={24} lg={12} className="center-align" style={{marginRight: 'auto', marginLeft: 'auto'}}>
+                  <ProgressiveImage preview={rank.tier_image} src={rank.tier_image} render={(src, style) => <Img className="responsive-img center-align" alt={rank.tier} src={src} style={style}/>}/>
+                  <Tooltip title={this.capitalize(rank.tier)} placement="bottom">
+                  <h3>{rank.comprank}</h3>
+                  </Tooltip>
+                </Col>
+                <Col md={24} lg={12}>
+                  <p><Avatar size="large" shape="square" src={rank.avatar}/></p>
+                  <p>Level: {rank.prestige}<Icon type="star"/> | {rank.level}</p>
+                  <p>Wins: {rank.wins}</p>
+                  <p>Total Games: {rank.games}</p>
+                  <p>Win Rate: {rank.win_rate}%</p>
+                </Col>
+              </Row>
             </Card>
           </Col>
           <Col sm={24} md={24} lg={12}>
-            Hero information will display here
+            <ResponsiveContainer height={600}>
+              <BarChart layout="vertical" data={heroes}>
+                <Bar dataKey="time" background={{fill: '#eee'}}>
+                  {
+                    heroes.map((e, index) => (
+                      <Cell key={`cell-${index}`} fill="#0d47a1"/>
+                    ))
+                  }
+                  <LabelList dataKey="minutes" position="end" formatter={v => `${v} hours`} />
+                </Bar>
+                <XAxis type="number" hide={true}/>
+                <YAxis type="category" dataKey="name" width={90}/>
+              </BarChart>
+            </ResponsiveContainer>
           </Col>
         </Row>
       </div>
@@ -84,4 +144,4 @@ class OWRank extends Component {
   }
 }
 
-export default OWRank;
+export default Form.create()(OWRank);
